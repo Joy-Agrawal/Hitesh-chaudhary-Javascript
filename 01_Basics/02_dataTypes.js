264a29e690c98fb617a90f8909bb24967c98b7df
Joy Agrawal <104222673+Joy-Agrawal@users.noreply.github.com>
2024-03-30 11:10:18 +0000

    added files

--- 01_Basics/02_dataTypes.js
+++ 01_Basics/02_dataTypes.js
@@ -3,3 +3,16 @@
 // alert("Hello")//this cannot be used in node js
 console.log(3+3)
 console.log('hitesh');
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value 
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null);//object;
+console.log(typeof undefined);//undefined
